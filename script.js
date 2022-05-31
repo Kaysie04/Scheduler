dd46c0e8e50e530741c404d5f32a7f9a4e2d8786
Kaysie <kaysielandersonwork@gmail.com>
2022-05-31 06:57:34 -0500

    added pseudocode to js for time-block color change logic

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 // displays current time
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-// set items to local storage
-$(".saveBtn").on("click", function () {
-    localStorage.setItem();
-})
+    // // this function will follow the hour of the day
+    // function hourDisplay() {
+    //     //get current number of hours.
+    //     var currentHour = moment().hour();
+
+    //         //check if we've moved past this time, click into css/html given classes of past, present, or future
+    //         if (timeBlockHour < currentHour) {
+    //             .addClass("past");
+    //             .removeClass("future");
+    //             .removeClass("present");
+    //         }
+    //         else if (blockHour === currentHour) {
+    //             .removeClass("past");
+    //             .addClass("present");
+    //             .removeClass("future");
+    //         }
+    //         else {
+    //             .removeClass("present");
+    //         .removeClass("past");
+    //         .addClass("future");
+    //         }
+    //     })
+    // }
+    // hourDisplay();
