f31fbb24eb567051960420966a0574348e47d5da
Kaysie <kaysielandersonwork@gmail.com>
2022-05-30 21:20:53 -0500

    added curent time display, created rows for each time and columns for each time, restyled some areas

---
+++ script.js
@@ -0,0 +1,7 @@
+// displays current time
+$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+// set items to local storage
+$(".saveBtn").on("click", function () {
+    localStorage.setItem();
+})
