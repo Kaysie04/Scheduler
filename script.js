0acc6cadd53d6e503bc4b3ea2c80e7c528831e28
Kaysie <kaysielandersonwork@gmail.com>
2022-05-31 21:08:38 -0500

    fixed column display

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
 // displays current time
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-     // this function will follow the hour of the day
-     function hourDisplay() {
+var currentTime = moment().hour()
+        console.log(currentTime)
 
-        //check if we've moved past this time, click into css/html given classes of past, present, or future
-        if (moment().isAfter(hour)) {
+    
+           
+           // set id's to 1-19
+           // data attributes
+           //data-hour = 
+           
+           if ( moment() > "#9" ) {
             $(".time-block").addClass("past");
             $(".time-block").removeClass("future");
             $(".time-block").removeClass("present");
-            
-          }
-
-          else if (moment().isBefore(hour)) {
+           }
+           else { if (moment() === "#9") 
             $(".time-block").removeClass("past");
             $(".time-block").addClass("future");
             $(".time-block").removeClass("present");
-
-              
-          } else {
+           }
+         
+        } else {
             $(".time-block").addClass("present");
             $(".time-block").removeClass("past");
             $(".time-block").removeClass("future");
-        }
-     }
-        hourDisplay();
 
 
-  
+        // this function will follow the hour of the day
+     function hourDisplay() {}
 
-
-
+     //check if we've moved past this time, click into css/html given classes of past, present, or future
+     
+     
+     
+     //$(".time-block").each (function () {
+        // $(this).
+        
       
  
