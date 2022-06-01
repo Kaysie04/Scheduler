4ae1b7ca02fcedd14731aee68b00e1ee34e9fb6c
Kaysie <kaysielandersonwork@gmail.com>
2022-06-01 14:29:31 -0500

    created if else statements to change color of rows

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-// displays current time
-$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-
-var currentTime = moment().hour()
-        console.log(currentTime)
-
+$(document).ready(function () {
     
-           
-           // set id's to 1-19
-           // data attributes
-           //data-hour = 
-           
-           if ( moment() > "#9" ) {
-            $(".time-block").addClass("past");
-            $(".time-block").removeClass("future");
-            $(".time-block").removeClass("present");
-           }
-           else { if (moment() === "#9") 
+    // displays current time
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+})
+
+
+    function hourBlocks() {
+
+    // get current time
+    var currentTime = moment().hour()
+
+    // if (currentTime.isBeforehourNine) {
+    //     console.log("true")
+    // }
+    // else {console.log("false")}
+   
+    
+    // hourNine
+     var hourNine =  moment({h:9})
+     $("#9").append(hourNine)
+     $("#9").text(hourNine)
+
+        if (currentTime.isBeforehourNine) {
             $(".time-block").removeClass("past");
-            $(".time-block").addClass("future");
             $(".time-block").removeClass("present");
+            $(".time-block").addClass("future");
+        }
+        else if (currentTime.isAfterhourNine)  {  
+            $("#9").addClass("past");
+            $("#9").removeClass("present");
+            $("#9").removeClass("future");
            }
          
-        } else {
-            $(".time-block").addClass("present");
-            $(".time-block").removeClass("past");
-            $(".time-block").removeClass("future");
+         else {
+            $("#9").removeClass("past");
+            $("#9").addClass("present");
+            $("#9").removeClass("future");
+         }
+    }
+    hourBlocks();
+
+    
 
 
-        // this function will follow the hour of the day
-     function hourDisplay() {}
+    
+    
+
+    
+
+        
+         // this function will follow the hour of the day
+         //function hourDisplay() {}
 
      //check if we've moved past this time, click into css/html given classes of past, present, or future
      
